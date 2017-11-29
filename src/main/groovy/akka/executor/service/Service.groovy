a58dc35e3c7abcd55112eef226f26fd895bc66c6
MarkKlimenko <22773@bk.ru>
2017-11-30 00:02:19 +1000

    Add sharding

--- src/main/groovy/akka/executor/service/Service.groovy
+++ src/main/groovy/akka/executor/service/Service.groovy
@@ -2,19 +2,26 @@ package akka.executor.service
 
 import akka.actor.ActorRef
 import akka.actor.ActorSystem
+import com.typesafe.config.Config
+import com.typesafe.config.ConfigFactory
 import groovy.util.logging.Slf4j
 
 @Slf4j
 class Service {
     static void main(String[] args) {
-        final ActorSystem system = ActorSystem.create("helloakka");
-        final ActorRef executorActor = system.actorOf(Executor.props(), "printerActor")
+        Config config = ConfigFactory
+                .parseString("akka.remote.netty.tcp.port=" + args[0])
+                .withFallback(ConfigFactory.load())
 
-        if(args[1] == 'main') {
-            5.times{
-                sleep(2000)
-                 log.info("Main ${args[0]} call actor")
-                executorActor.tell("${args[0]} : ${it}", ActorRef.noSender())
+        ActorSystem system = ActorSystem.create("ShardingSystem", config)
+        ActorRef executorActor = system.actorOf(Executor.props(), "executorActor")
+
+        if(args[1] == 'master') {
+            int i = 0
+            while(true) {
+                sleep(4000)
+                 log.info("Master ${args[0]} call actor")
+                executorActor.tell("${args[0]} : ${i++}", ActorRef.noSender())
             }
         }
     }
